8b685e01ffd14533f8f862ee126c93cb9c9599ba
xiezhenfeng <xiezhenfeng@58ganji.com>
2018-11-20 17:04:39 +0800

    创建项目

---
+++ Flutter_test/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  Flutter_test
+//
+//  Created by 58 on 2018/11/19.
+//  Copyright © 2018年 58. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Flutter/Flutter.h>
+
+@interface AppDelegate : FlutterAppDelegate <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Flutter_test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flutter_test
+//
+//  Created by 58 on 2018/11/19.
+//  Copyright © 2018年 58. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
